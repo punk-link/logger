f17aa0d9a2b077a5754cb05381364aabb1edc851
Kirill Taran <kirilltaran@hotmail.com>
2022-10-15 19:30:22 +0400

    Environment variable support added

--- logger.go
+++ logger.go
@@ -1,15 +1,45 @@
 package logger
 
 import (
+	"os"
+	"strings"
+
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/rs/zerolog/pkgerrors"
+
+	envManaber "github.com/punk-link/environment-variable-manager"
 )
 
 type Logger struct{}
 
 func init() {
 	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
+
+	logLevel := getLogLevel()
+	switch strings.ToLower(logLevel) {
+	case "debug":
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	case "trace":
+		zerolog.SetGlobalLevel(zerolog.TraceLevel)
+	case "info":
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	case "warn":
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	case "error":
+		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+	case "fatal":
+		zerolog.SetGlobalLevel(zerolog.FatalLevel)
+	}
+
+	host, err := os.Hostname()
+	if err != nil {
+		log.Logger = log.With().Str("host", "unknown").Logger()
+	} else {
+		log.Logger = log.With().Str("host", host).Logger()
+	}
+
+	log.Logger = log.With().Stack().Caller().Logger()
 }
 
 func (logger *Logger) LogError(err error, format string, args ...interface{}) {
@@ -30,4 +60,13 @@ func (logger *Logger) LogWarn(format string, args ...interface{}) {
 
 func (logger *Logger) Printf(format string, values ...interface{}) {
 	log.Printf(format, values...)
+}
+
+func getLogLevel() string {
+	isExist, level := envManaber.TryGetEnvironmentVariable("LOG_LEVEL")
+	if !isExist {
+		return "Error"
+	}
+
+	return level
 }
